7f49c831c1cd8a68f09e221e4be733a26ddffe47
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-11-23 17:20:13 +0100

    change auth source

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL+"karafun").then(() => {
+mongoose.connect(process.env.MONGO_URL+"karafun?authSource=admin").then(() => {
     console.log("Connected to MongoDB");
-}).catch((err) => { 
+}).catch((err) => {
     console.log("Error connecting to MongoDB", err);
 });
 
